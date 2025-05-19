bdc8441d2d79d50bd205953e3972c63dc5461393
Mohammadbilal90 <bilal.asif@centricdxb.com>
2025-05-19 23:21:24 +0500

    creating main content in this website

---
+++ js/app.js
@@ -0,0 +1,31 @@
+AOS.init();
+
+
+
+
+
+$('.slider-slick').slick({
+  centerMode: true,
+  centerPadding: '90px',
+  slidesToShow: 4,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 4
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
